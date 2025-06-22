269141cd566f69067b2f4f228cb4764317011cf2
Taehoon <cjsxogns0518@gmail.com>
2025-06-22 22:18:22 +0900

    인프런[Sorting and Searching(정렬, 이분검색과 결정알고리즘)] - 버블정렬 (정답률 49%)

---
+++ 인프런 강의/Sorting and Searching(정렬, 이분검색과 결정알고리즘)/버블정렬/Code.java
@@ -0,0 +1,52 @@
+// 나의 풀이
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int [] arr = new int[n];
+        for (int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i=n-1; i>0; i--){
+            for (int j=0; j<i; j++){
+                if (arr[j]>arr[j+1]){
+                    int tmp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = tmp;
+                }
+            }
+        }
+
+        for (int x : arr){
+            System.out.print(x + " ");
+        }
+    }
+}
+
+// 다른 사람 풀이
+import java.util.*;
+class Main {
+    public int[] solution(int n, int[] arr){
+        for(int i=0; i<n-1; i++){
+            for(int j=0; j<n-i-1; j++){
+                if(arr[j]>arr[j+1]){
+                    int tmp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=tmp;
+                }
+            }
+        }
+        return arr;
+    }
+    public static void main(String[] args){
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        int n=kb.nextInt();
+        int[] arr=new int[n];
+        for(int i=0; i<n; i++) arr[i]=kb.nextInt();
+        for(int x : T.solution(n, arr)) System.out.print(x+" ");
+    }
+}
